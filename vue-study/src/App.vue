2d86529aa3c643009c7d05fab5e0ea218d873a59
Nick <godgiven.nick@gmail.com>
2021-10-22 10:24:25 +0300

    #15 | Ошибка, если тикер уже добавлен

--- vue-study/src/App.vue
+++ vue-study/src/App.vue
@@ -1,4 +1,14 @@
 <template>
+
+  <div 
+    v-if="preloader"
+    class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
+    <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+  </div>
+
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
     <div class="container">
       <div class="w-full my-4"></div>
@@ -19,6 +29,8 @@
                 placeholder="Например DOGE"
               />
             </div>
+
+            <div v-if="error" class="text-sm text-red-600">Такой тикер уже добавлен</div>
           </div>
         </div>
         <button
@@ -143,6 +155,8 @@ export default {
       ticker: '',
       tickers: [],
       graph: [],
+      preloader: true,
+      error: false,
 
       sel: null,
       apiKey:
@@ -155,6 +169,11 @@ export default {
       const currentTicker = {
         name: this.ticker,
         price: '-',
+      }
+
+      this.checkTicker(this.ticker)
+      if(this.error){
+        return;
       }
 
       this.tickers.push(currentTicker)
@@ -188,6 +207,18 @@ export default {
       this.graph = []
     },
 
+    showError(){
+      this.error = true;
+    },
+
+    checkTicker(name){
+      this.tickers.find(function(t){
+        if( t.name === name ){
+          this.error = true;
+        }
+      }, this)
+    },
+
     normalizeGraph() {
       const maxValue = Math.max(...this.graph)
       const minValue = Math.min(...this.graph)
@@ -195,6 +226,10 @@ export default {
         (price) => 5 + (price - minValue) * 95 / (maxValue - minValue),
       )
     },
+  },
+
+  mounted: function(){
+    this.preloader = false
   },
 }
 </script>
